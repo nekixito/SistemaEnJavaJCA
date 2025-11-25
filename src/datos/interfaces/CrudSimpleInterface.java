a5b45fa64ab5bdc7c55a05a0d256e7223b18c2c3
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-24 18:00:57 -0600

    Se agrega interfaz para CRUD simple.

---
+++ src/datos/interfaces/CrudSimpleInterface.java
@@ -0,0 +1,17 @@
+package datos.interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author Miguel
+ */
+public interface CrudSimpleInterface<T> {
+    public List<T> listar(String texto);
+    public boolean insertar(T obj);
+    public boolean actualizar(T obj);
+    public boolean desactivar(int id);
+    public boolean activar(int id);
+    public int total();
+    public boolean existe(String texto);
+}
