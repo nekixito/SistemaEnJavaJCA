4154750a3e87a2c6e841e56d94fffd51b01d67f9
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-30 15:11:07 -0600

    Se implementa método listar.

--- src/datos/CategoriaDAO.java
+++ src/datos/CategoriaDAO.java
@@ -6,10 +6,13 @@ package datos;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import database.Conexion;
 import datos.interfaces.CrudSimpleInterface;
 import entidades.Categoria;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -28,7 +31,29 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria>{
 
     @Override
     public List<Categoria> listar(String texto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Categoria> registros = new ArrayList();
+        
+        try {
+            ps = CON.conectar().prepareStatement("SELECT * FROM categoria WHERE nombre LIKE ?");
+            ps.setString(1, "%" + texto + "%");
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+                registros.add(new Categoria( rs.getInt(1), rs.getString(2), rs.getString(3), rs.getBoolean(4)));
+            }
+            
+            ps.close();
+            rs.close();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally{
+            ps = null;
+            rs = null;
+            CON.desconectar();
+        }
+        
+        return registros;
     }
 
     @Override
