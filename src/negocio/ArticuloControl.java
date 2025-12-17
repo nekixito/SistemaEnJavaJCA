b9b75f069c7f5a3edcbfe940542c5b3501738f69
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-17 12:14:46 -0600

    Lógica de negocio - Artículo

---
+++ src/negocio/ArticuloControl.java
@@ -0,0 +1,142 @@
+package negocio;
+
+import datos.ArticuloDAO;
+import entidades.Articulo;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Miguel
+ */
+public class ArticuloControl {
+
+    private final ArticuloDAO DATOS;
+    private Articulo obj;
+    private DefaultTableModel modeloTabla;
+    public int registrosMostrados;
+
+    public ArticuloControl() {
+        this.DATOS = new ArticuloDAO();
+        this.obj = new Articulo();
+        this.registrosMostrados = 0;
+    }
+
+    public DefaultTableModel listar(String texto, int totalPorPagina, int numPagina) {
+        List<Articulo> lista = new ArrayList();
+        lista.addAll(DATOS.listar(texto, totalPorPagina, numPagina));
+
+        String[] titulos = {"id", "Categoria Id", "Categoria", "Código", "Nombre", "Precio Venta", "Stock", "Descripcion", "Imagen", "Estado"};
+        this.modeloTabla = new DefaultTableModel(null, titulos);
+
+        String estado;
+        String[] registro = new String[10];
+
+        this.registrosMostrados = 0;
+        for (Articulo item : lista) {
+            if (item.isActivo()) {
+                estado = "Activo";
+            } else {
+                estado = "Inactivo";
+            }
+
+            registro[0] = Integer.toString(item.getId());
+            registro[1] = Integer.toString(item.getCategoriaId());
+            registro[2] = item.getCategoriaNombre();
+            registro[3] = item.getCodigo();
+            registro[4] = item.getNombre();
+            registro[5] = Double.toString(item.getPrecioVenta());
+            registro[6] = Integer.toString(item.getStock());
+            registro[7] = item.getDescripcion();
+            registro[8] = item.getImagen();
+            registro[9] = estado;
+
+            this.modeloTabla.addRow(registro);
+            this.registrosMostrados = this.registrosMostrados + 1;
+        }
+
+        return this.modeloTabla;
+    }
+
+    public String insertar(int categoriaId, String codigo, String nombre, double precioVenta, int stock, String descripcion, String imagen) {
+        if (DATOS.existe(nombre)) {
+            return "El registro ya existe.";
+        } else {
+            obj.setCategoriaId(categoriaId);
+            obj.setCodigo(codigo);
+            obj.setNombre(nombre);
+            obj.setPrecioVenta(precioVenta);
+            obj.setStock(stock);
+            obj.setDescripcion(descripcion);
+            obj.setImagen(imagen);
+            if (DATOS.insertar(obj)) {
+                return "OK";
+            } else {
+                return "Error en la insercion del registro";
+            }
+        }
+    }
+
+    public String actualizar(int id, int categoriaId, String codigo, String nombre, String nombreAnt, double precioVenta, int stock, String descripcion, String imagen) {
+        if (nombre.equals(nombreAnt)) {
+            obj.setId(id);
+            obj.setCategoriaId(categoriaId);
+            obj.setCodigo(codigo);
+            obj.setNombre(nombre);
+            obj.setPrecioVenta(precioVenta);
+            obj.setStock(stock);
+            obj.setDescripcion(descripcion);
+            obj.setImagen(imagen);
+
+            if (DATOS.actualizar(obj)) {
+                return "OK";
+            } else {
+                return "Error en la actualizacion";
+            }
+        } else {
+            if (DATOS.existe(nombre)) {
+                return "El registro ya existe";
+            } else {
+                obj.setId(id);
+                obj.setCategoriaId(categoriaId);
+                obj.setCodigo(codigo);
+                obj.setNombre(nombre);
+                obj.setPrecioVenta(precioVenta);
+                obj.setStock(stock);
+                obj.setDescripcion(descripcion);
+                obj.setImagen(imagen);
+                if (DATOS.actualizar(obj)) {
+                    return "OK";
+                } else {
+                    return "Error en la actualizacion";
+                }
+            }
+        }
+    }
+
+    public String desactivar(int id) {
+        if (DATOS.desactivar(id)) {
+            return "OK";
+        } else {
+            return "No se puede desactivar el registro";
+        }
+    }
+
+    public String activar(int id) {
+        if (DATOS.activar(id)) {
+            return "OK";
+        } else {
+            return "No se puede activar el registro";
+        }
+    }
+
+    public int total() {
+        return DATOS.total();
+    }
+
+    public int totalMostrados() {
+        return this.registrosMostrados;
+    }
+
+}
